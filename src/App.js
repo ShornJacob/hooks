72ef46ba29ea78b2a3a12317d69cd635c4861fd4
Shorn Jacob <shorn@transparency.consulting>
2019-10-30 16:26:23 +1300

    USe state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {TextField} from '@material-ui/core';
 
 function App() {
+
+  const [name, setName ] = useState("Mary")
+
+  function handleNameChange(e) {
+    setName(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section>
+       <TextField
+          id="standard-basic"
+          label="Name"
+          margin="normal"
+          value={name}
+          onChange={handleNameChange}
+        />
+    </section>
+  )
 }
 
 export default App;

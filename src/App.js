0233f73c89a25cc62752f41664997a7467a1e33f
Shorn Jacob <shorn@transparency.consulting>
2019-10-31 18:13:23 +1300

    USe of context

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import {TextField} from '@material-ui/core';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import {Container} from '@material-ui/core';
+import LocaleContext from './LocaleContext';
+import { InputLabel } from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    // flexWrap: 'wrap',
+    flexDirection : 'column',
+    alignItems: 'center'
+  },
+
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+}));
+
+
 
 function App() {
 
+
+
+  console.log(useTheme())
+
   const [name, setName ] = useState("Mary")
+  const [surname, setSurname ] = useState("Poppins")
+
+  const locale = useContext(LocaleContext)
 
   function handleNameChange(e) {
     setName(e.target.value)
   }
 
+  function handleSurnameChange(e) {
+    setSurname(e.target.value)
+  }
+
+  const classes = useStyles();
+
   return (
+    <React.Fragment>
+      <CssBaseline />
+      <LocaleContext.Provider value="en">
+      <Container maxWidth="sm">
+     
+   
     <section>
+    <div className={classes.container}>
        <TextField
-          id="standard-basic"
-          label="Name"
-          margin="normal"
+          // id="standard-basic"
+          // label="Name"
+          // margin="normal"
           value={name}
           onChange={handleNameChange}
+          className={classes.textField}
         />
+
+       <TextField
+          // id="standard-basic"
+          // label="Name"
+          // margin="normal"
+          value={surname}
+          onChange={handleSurnameChange}
+          className={classes.textField}
+        />
+
+        <InputLabel>{locale}</InputLabel>
+        </div>
     </section>
+  
+  
+
+    </Container>
+    </LocaleContext.Provider>
+    </React.Fragment>
   )
 }
 
 export default App;
+
+
+//https://css-tricks.com/almanac/properties/f/flex-wrap/
+//flex-wrap: wrap;
+//Specifies that the flexible items will wrap if necessary
+
+//The container centers your content horizontally. It's the most basic layout element.
+
+//https://www.w3schools.com/csSref/css3_pr_flex-direction.asp
+//column	The flexible items are displayed vertically, as a column
+
+//https://www.w3schools.com/cssref/css3_pr_align-items.asp
+
+
+//Material-UI uses a recommended 8px scaling factor by default.
+//https://material-ui.com/customization/spacing/
+//Most measurements align to an 8dp grid applied, which aligns both spacing and the overall layout.
+
+//The <section> tag is new in HTML5.
---
+++ src/LocaleContext.js
@@ -0,0 +1,3 @@
+import React from 'react';
+const LocaleContext = React.createContext("en");
+export default LocaleContext
